602cd81f7aa7eaefc84e7d86bfd403396da7a83b
Gracicarlos <grasealka01@gmail.com>
2018-10-15 15:43:50 -0300

    5 sentidos

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,6 +3,7 @@ package main;
 public class Main {
 public static void main (String [] args) {
 	
-System.out.println("observando a vida apartir dos olhos de uma deficiente visual");
+System.out.println("observando a vida apartir dos olhos de uma deficiente visual"+"\n"+"não enchergar com os olhos, não quer dizer que você não possa continuar enchergando com os demais sentidos");
+
 }
 }
